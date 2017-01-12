54f568a17ff5e31f7fce70d4144232536de41bc6
luigi055 <luigi5021@gmail.com>
2017-01-12 11:25:20 -0400

    React and Redux added and optimized boilerplate

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 import 'font-awesome/scss/font-awesome.scss';
 import './scss/style.scss';
-const name = 'Pedro La Rosa';
 
-console.log(`Hello ${name} this was written in ES6`);
+console.log('React/Redux Boilerplate ready to go!');
+
+ReactDOM.render(
+  <div className="container">
+    <h1 className="text-success title-component">React Redux Bootstrapv4 Boilerplate</h1>
+    <ul className="list-group list-unstyled">
+      <li className="list-group-item-success">React Redux <i className="fa fa-thumbs-up" /></li>
+      <li className="list-group-item-success">Bootstrap 4 alpha-6 scss <i className="fa fa-thumbs-up" /></li>
+      <li className="list-group-item-success">FontAwesome 4.7 <i className="fa fa-thumbs-up" /></li>
+      <li className="list-group-item-success">PostCSS autoprefixer Plugin <i className="fa fa-thumbs-up" /></li>
+      <li className="list-group-item-success">ES6 + stage-0 <i className="fa fa-thumbs-up" /></li>
+    </ul>
+  </div>,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
         loader: 'babel',
         exclude: /(node_modules|bower_components)/,
         query: {
-          presets: ['es2015']
+          presets: ['es2015', 'react', 'stage-0']
         }
       },
       {
