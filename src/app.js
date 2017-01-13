9b9d919647894faff7adbbc8a08e5aa52c352f37
luigi055 <luigi5021@gmail.com>
2017-01-12 22:47:19 -0430

    revert "Added React-Router and config redux-devtools in store"
    This reverts commit 611395537993eff478116ccfbe33128896aee9d3.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import { Provider } from 'react-redux';
-import { Route, Router, hashHistory, IndexRoute } from 'react-router';
 
 import 'font-awesome/scss/font-awesome.scss';
 import './scss/style.scss';
